002f955286a9f748844ff1b3c78943af99ec76b9
DObbyK <dobbyk@techie.com>
2020-12-21 21:03:43 +0000

    Changed how we are storing units.

---
+++ data/rulesets/default/units/units.py
@@ -0,0 +1,7 @@
+# This is the terrian rules for the default ruleset, this is how its setup:
+# class: unit type
+# 	{unitname} = [{Acorym}, {A}, {D}, {HP}, {MP}, {TECH}, {PRODUCTION}]
+
+class melee:
+	warrior = ['WR', 1, 1, 10, 2, 'NONE', 10]
+	
